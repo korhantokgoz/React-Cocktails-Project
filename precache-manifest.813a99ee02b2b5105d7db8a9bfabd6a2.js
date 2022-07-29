3b386162fc1b09dbf7b6c1eb19437afacaf88c18
korhantokgoz <korhan.tokgoz92@gmail.com>
2022-07-29 10:42:00 +0300

    Updates

---
+++ precache-manifest.813a99ee02b2b5105d7db8a9bfabd6a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0e9633f8863f37eaec002d0d51cfa6",
+    "url": "/React-Cocktails-Project/index.html"
+  },
+  {
+    "revision": "0f1bbe50aa15e896b391",
+    "url": "/React-Cocktails-Project/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "236a9a53deaeafab3924",
+    "url": "/React-Cocktails-Project/static/js/2.4239b459.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Cocktails-Project/static/js/2.4239b459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1bbe50aa15e896b391",
+    "url": "/React-Cocktails-Project/static/js/main.99c0562c.chunk.js"
+  },
+  {
+    "revision": "957050eda4f86355c324",
+    "url": "/React-Cocktails-Project/static/js/runtime-main.30860c82.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/React-Cocktails-Project/static/media/logo.9a3d2645.svg"
+  }
+]);
